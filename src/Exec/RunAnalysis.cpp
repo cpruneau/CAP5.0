b7da428d237b49dd8a0453fd6fbbc5e81ecb0229
Claude Pruneau <34493579+cpruneau@users.noreply.github.com>
2023-10-11 13:04:48 -0400

    Update RunAnalysis.cpp

--- src/Exec/RunAnalysis.cpp
+++ src/Exec/RunAnalysis.cpp
@@ -233,7 +233,7 @@ void RunAnalysis::configure()
   configureLabels();
   String  histoImportPath = getValueString("HistogramsImportPath");
   String  histoExportPath = getValueString("HistogramsExportPath");
-
+  int nBunches = getValueInt("Analysis:nBunches");
   if (reportDebug(__FUNCTION__)) printConfiguration(cout);
 
   //
