ee303c782465322b7e16b7fffcc937d6ebb97ce8
Claude Pruneau <34493579+cpruneau@users.noreply.github.com>
2023-12-15 11:20:34 -0500

    Update hello.C

--- src/Macros/hello.C
+++ src/Macros/hello.C
@@ -13,13 +13,13 @@ void hello()
 void loadBase(const TString & includeBasePath)
 {
   TString includePath = includeBasePath + "/Base/";
-  gSystem->Load(includePath+"Exceptions.hpp");
-  gSystem->Load(includePath+"Timer.hpp");
-  gSystem->Load(includePath+"MessageLogger.hpp");
-  gSystem->Load(includePath+"Task.hpp");
-  gSystem->Load(includePath+"Collection.hpp");
-  gSystem->Load(includePath+"HistogramCollection.hpp");
-  gSystem->Load(includePath+"HistogramGroup.hpp");
+//  gSystem->Load(includePath+"Exceptions.hpp");
+//  gSystem->Load(includePath+"Timer.hpp");
+//  gSystem->Load(includePath+"MessageLogger.hpp");
+//  gSystem->Load(includePath+"Task.hpp");
+//  gSystem->Load(includePath+"Collection.hpp");
+//  gSystem->Load(includePath+"HistogramCollection.hpp");
+//  gSystem->Load(includePath+"HistogramGroup.hpp");
   gSystem->Load("libBase.so");
   //gSystem->Load("libBase.dylib");
 }
