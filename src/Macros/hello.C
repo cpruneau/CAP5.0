007807dbd05afe8e25fe2fb1c68a70e9b74cd5a7
Claude Pruneau <34493579+cpruneau@users.noreply.github.com>
2023-12-15 11:08:24 -0500

    testing WSU grid

--- src/Macros/hello.C
+++ src/Macros/hello.C
@@ -1,4 +1,24 @@
+
+void loadBase(const TString & includeBasePath);
+
 void hello()
 {
+  TString includeBasePath = getenv("CAP_SRC");
+  loadBase(includeBasePath);
+
   cout << "Hello World" << endl;
+}
+
+
+void loadBase(const TString & includeBasePath)
+{
+  TString includePath = includeBasePath + "/Base/";
+  gSystem->Load(includePath+"Exception.hpp");
+  gSystem->Load(includePath+"Timer.hpp");
+  gSystem->Load(includePath+"MessageLogger.hpp");
+  gSystem->Load(includePath+"Task.hpp");
+  gSystem->Load(includePath+"Collection.hpp");
+  gSystem->Load(includePath+"HistogramCollection.hpp");
+  gSystem->Load(includePath+"HistogramGroup.hpp");
+  gSystem->Load("libBase.dylib");
 }
