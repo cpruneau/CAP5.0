449c256ee1f7b4e86bd4329df38a8e212cd7d57a
Claude Pruneau <34493579+cpruneau@users.noreply.github.com>
2024-03-15 10:40:06 -0400

    Bunch -> BUNCH in RunAnalysis.cpp

--- src/Macros/RunAna.C
+++ src/Macros/RunAna.C
@@ -42,7 +42,7 @@ void loadTherminator(const TString & includeBasePath);
 //! nSubbunchesPerBunch : number of sub-bunches  (must be 1 on grid)
 //! nBunches :  number of bunches  (must be 1 on grid)
 //!
-int RunAna(TString configFile="Therminator/AnalysisTherminator_bwT180_CH_Y10.ini",
+int RunAna(TString configFile="Therminator/AnalysisTherminator_bwT150_CH_Y10.ini",
            TString histogramPath="./therminatorTest/",
            long seed=1121331,
            bool isGrid=false,
